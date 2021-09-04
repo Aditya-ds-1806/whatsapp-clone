0ebc5c6dbf2eb9ee2402bede092d5b453cbad6b3
Aditya DS <adithya18062000@gmail.com>
2021-09-04 16:09:00 +0530

    setup sign out

--- public/src/auth.js
+++ public/src/auth.js
@@ -1,16 +1,17 @@
 import {
-    getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged,
+    getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut,
 } from 'firebase/auth';
 import app from './initApp';
 
-const singInBtn = document.querySelector('#signIn');
-const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 const welcome = document.querySelector('#welcome');
 const chat = document.querySelector('#chat');
 const spinner = document.querySelector('.spinner-border');
+const signInBtn = document.querySelector('#signIn');
+const signOutBtn = document.querySelector('#signOut');
 
 const signInHandler = async () => {
+    const provider = new GoogleAuthProvider();
     try {
         const result = await signInWithPopup(auth, provider);
         const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -22,15 +23,28 @@ const signInHandler = async () => {
     }
 };
 
+const signOutHandler = async () => {
+    try {
+        await signOut(auth);
+        welcome.classList.remove('d-none');
+        chat.classList.add('d-none');
+        signInBtn.classList.remove('d-none');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 onAuthStateChanged(auth, (user) => {
+    spinner.classList.add('d-none');
     if (user) {
         welcome.classList.add('d-none');
         chat.classList.remove('d-none');
     } else {
         welcome.classList.remove('d-none');
         chat.classList.add('d-none');
-        spinner.classList.add('d-none');
-        singInBtn.classList.remove('d-none');
-        singInBtn.addEventListener('click', signInHandler);
+        signInBtn.classList.remove('d-none');
     }
 });
+
+signInBtn.addEventListener('click', signInHandler);
+signOutBtn.addEventListener('click', signOutHandler);
