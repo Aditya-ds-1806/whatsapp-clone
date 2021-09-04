e3ab54b7cbc5153b3509d5dc3b9944ba1f0713c8
Aditya DS <adithya18062000@gmail.com>
2021-09-05 02:41:38 +0530

    start observing messages on login

--- public/src/auth.js
+++ public/src/auth.js
@@ -3,7 +3,7 @@ import {
 } from 'firebase/auth';
 import app from './initApp';
 import {
-    addUser, handleDisconnection, setStatus, observeUsers,
+    addUser, handleDisconnection, setStatus, observeUsers, observeMessages,
 } from './db';
 import { hideSpinner, showChat, showWelcome } from './helpers';
 
@@ -32,9 +32,12 @@ const signOutHandler = async () => {
     }
 };
 
+const getCurrentUser = () => auth.currentUser;
+
 onAuthStateChanged(auth, async (user) => {
     if (user) {
         await setStatus(user.uid, true);
+        await observeMessages(user.uid);
         observeUsers(user.uid);
         handleDisconnection(user.uid);
         showChat();
@@ -46,3 +49,5 @@ onAuthStateChanged(auth, async (user) => {
 
 signInBtn.addEventListener('click', signInHandler);
 signOutBtn.addEventListener('click', signOutHandler);
+
+export default getCurrentUser;
