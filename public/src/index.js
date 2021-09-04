ccb067effc8deba5b7c6b90c11bac07f48ea4e7a
Aditya DS <adithya18062000@gmail.com>
2021-09-05 02:46:56 +0530

    add message to DB on enter

--- public/src/index.js
+++ public/src/index.js
@@ -1,16 +1,15 @@
 import './initApp';
-import './auth';
-import { createNewMessage } from './helpers';
+import getCurrentUser from './auth';
+import { addMessage } from './db';
 
 const form = document.querySelector('form');
-const messages = document.querySelector('#messages');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const message = document.querySelector('input').value;
-    if (message.trim() === '') return;
-    const messageDiv = createNewMessage(message, false);
-    document.querySelector('input').value = '';
-    messages.append(messageDiv);
-    messages.scrollTop = messages.scrollHeight;
+    const message = document.querySelector('input').value.trim();
+    const receiver = document.querySelector('#messages').dataset.uid;
+    const sender = getCurrentUser().uid;
+    if (message === '') return;
+    const data = { sender, receiver, message };
+    await addMessage(data);
 });
