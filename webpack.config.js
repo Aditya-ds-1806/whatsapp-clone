1901b54ec572f7e0e12fdbfc0a0e98f7e586de5a
Aditya DS <adithya18062000@gmail.com>
2021-09-05 17:32:23 +0530

    setup css loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
-    entry: path.resolve(__dirname, 'public/src/index.js'),
+    entry: path.resolve(__dirname, 'src/scripts/index.js'),
     output: {
-        path: path.resolve(__dirname, 'public/dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
     },
+    module: {
+        rules: [{
+            test: /\.(scss)$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        }],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css',
+        }),
+    ],
 };
