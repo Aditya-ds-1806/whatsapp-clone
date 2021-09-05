aaffda7a5496ecc46ee91ea178df3fa1631a8ceb
Aditya DS <adithya18062000@gmail.com>
2021-09-05 11:26:25 +0530

    set status 0 while sending message

--- public/src/index.js
+++ public/src/index.js
@@ -10,6 +10,8 @@ form.addEventListener('submit', async (e) => {
     const receiver = document.querySelector('#messages').dataset.uid;
     const sender = getCurrentUser().uid;
     if (message === '') return;
-    const data = { sender, receiver, message };
+    const data = {
+        sender, receiver, message, status: '0',
+    };
     await addMessage(data);
 });
