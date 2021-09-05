8d8a67b5ddcd3af726f086c6edb7cb362a7ad22d
Aditya DS <adithya18062000@gmail.com>
2021-09-05 17:04:34 +0530

    handle null message error

--- public/src/chat.js
+++ public/src/chat.js
@@ -5,6 +5,7 @@ const db = getDatabase(app);
 
 const markMessagesStatus = (messages, convId, currentUid, state) => {
     const updates = {};
+    if (!messages) return null;
     Object.entries(messages).forEach(async ([chatId, { receiver, status }]) => {
         if (receiver === currentUid && status === `${Number(state) - 1}`) {
             updates[`${chatId}/status`] = state;
